49c0f39947c98cb245f6600768fef25f4a1287d1
MFabrice001 <m.fabrice001@gmail.com>
2024-10-20 13:49:15 +0200

    updated

--- weather-dashboard/src/components/Navbar.js
+++ weather-dashboard/src/components/Navbar.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './App.css'; 
 
 function Navbar() {
   return (
-    <nav className="navbar bg-gray-800 p-4">
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center">
-          <h1 className="text-white text-2xl font-bold">Weather Dashboard</h1>
-          <ul className="flex space-x-4">
-            <li>
-              <Link to="/" className="text-white">Home</Link>
-            </li>
-            <li>
-              <Link to="/weather" className="text-white">Weather</Link>
-            </li>
-            <li>
-              <Link to="/signup" className="text-white">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/about" className="text-white">About</Link>
-            </li>
-          </ul>
-        </div>
+    <nav className="navbar">
+      <div className="container">
+        <h1>Weather Dashboard</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/weather">Weather</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
       </div>
     </nav>
   );
